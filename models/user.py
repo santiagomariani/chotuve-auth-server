ce41f388eef49f4231d23e0bd4c8417aa82d014f
Santiago Mariani <santiagomariani2@gmail.com>
2020-05-21 00:41:12 -0300

    agrego la propiedad display_name remplazando first_name y last_name y agrego la propiedad admin para el usuario

--- models/user.py
+++ models/user.py
@@ -5,14 +5,14 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True, nullable=False)
-    first_name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(50))
+    display_name = db.Column(db.String(30), nullable=False)
     phone_number = db.Column(db.String(50))
     image_location = db.Column(db.String(2083))
+    admin = db.Column(db.Boolean)
 
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'email', 'first_name', 'last_name', 'phone_number', 'image_location')
+        fields = ('id', 'email', 'display_name', 'phone_number', 'image_location')
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
