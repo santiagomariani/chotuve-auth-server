058c540e11e254336dabf6780c95b82adc0316d7
Santiago Mariani <santiagomariani2@gmail.com>
2020-05-30 12:12:13 -0300

    parece estar funcionando la base datos de testing [no-ci]

--- test/test_api.py
+++ test/test_api.py
@@ -2,22 +2,14 @@ import pytest
 from test.conftest import *
 from models.models import User
 
-"""
-def sum(x, y):
-    return x + y
-
-def test_answer():
-    assert sum(3, 4) == 7
-
-def test_home_page(testapp):
-    response = testapp.post('/ping')
-    assert response.status_code == 200
-    user = User.query.filter_by(email="sebastianperez@gmail.com").first()
-    assert user
-    print(user.display_name)
-"""
-
-def test_simple_ping(testapp):
-    response = testapp.post('/reset-codes')
+def test_simple_ping(db_handle, testapp):
+    user = User(email="santiagomariani2@gmail.com",
+                display_name="Santiago Mariani",
+                phone_number="2254405503",
+                image_location="http://www.google.com.ar",
+                admin=False)
+    db_handle.session.add(user)
+    db_handle.session.commit()
+    response = testapp.post('/reset-codes', json={'email':"santiagomariani2@gmail.com"})
     print(response.data)
     assert response.status_code == 200
