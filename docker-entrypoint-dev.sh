ef360fe350a09b8a15d6b3af33b599d303db0813
Santiago Mariani <santiagomariani2@gmail.com>
2020-05-30 18:15:08 -0300

    pequeño cambio para que corra los tests bien el travis

--- docker-entrypoint-dev.sh
+++ docker-entrypoint-dev.sh
@@ -7,12 +7,10 @@ then
     done
 
     echo "PostgreSQL started"
+
 fi
 
 python manage.py db init
-
 python manage.py db migrate
-
 python manage.py db upgrade
-
 python manage.py runserver -h 0.0.0.0 -p 5000
