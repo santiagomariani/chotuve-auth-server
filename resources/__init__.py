d6b1b96bbf79a52daab0c44d2531484b8aaa1a1f
Santiago Mariani <santiagomariani2@gmail.com>
2020-07-18 17:08:48 -0300

    update

--- resources/__init__.py
+++ resources/__init__.py
@@ -1,10 +1,11 @@
-from resources.users_routes import UsersRoutes, UniqueUserRoutes, UserIdFromTokenRoute
+from resources.users_routes import UsersRoutes, UniqueUserRoutes, UserIdFromTokenRoute, UserAdminRoute
 from resources.reset_codes import ResetCodesRoutes, ChangePasswordRoutes
 
 def register_routes(api):
     api.add_resource(UsersRoutes, '/users')
     api.add_resource(UniqueUserRoutes, '/users/<int:user_id>')
     api.add_resource(UserIdFromTokenRoute, '/users/id')
+    api.add_resource(UserAdminRoute, '/users/admin')
 
     api.add_resource(ResetCodesRoutes, '/reset-codes')
     api.add_resource(ChangePasswordRoutes, '/change-password-with-reset-code')
